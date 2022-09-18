2b41cfd7443381f5d7d39399512a122f9805ecf7
Sabbir Nasir <62333547+sabbirn26@users.noreply.github.com>
2022-09-18 05:56:05 +0000

    First code

--- main.swift
+++ main.swift
@@ -1,16 +1,3 @@
 import Foundation
-//playing with optional try
-
-enum UserError : Error{
-  case badID, networkError
-}
-
-func getUser(id : Int) throws -> String{
-  throws UserError.networkError
-}
-
-if let user = try? getUser(id: 26)
-{
-  print("User id is \(user)")
-}
-
+//first code
+print("Hello There!")
