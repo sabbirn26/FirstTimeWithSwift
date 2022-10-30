f13d4ddbb0f6cc17dc9e27320214888824038e5b
Sabbir Nasir <62333547+sabbirn26@users.noreply.github.com>
2022-10-30 07:15:23 +0000

    Array Higher Function

---
+++ SwiftHigherFunctions/ArrayRelated/13_ArraySwapAt.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+var arr = [10, 3, 20, 7, 8 , 11, 26]
+print("Actual array: \(arr)")
+
+//swap two elements in array
+arr.swapAt(1,2)
+print("After reversing: \(arr)")
+
+var arr1 = ["Java","Python","Red","Blue","Green","Purple","Black"]
+print("Before swaping: \(arr1)")
+//swap two elements in array
+arr1.swapAt(1,2)
+print("After swaping: \(arr1)")
