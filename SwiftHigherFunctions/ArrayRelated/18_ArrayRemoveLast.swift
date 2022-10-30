3822626d9262a8b31480142d58848978f9b223a9
Sabbir Nasir <62333547+sabbirn26@users.noreply.github.com>
2022-10-30 09:20:31 +0000

    Array Higher Function

--- SwiftHigherFunctions/ArrayRelated/18_ArrayRemoveLast.swift
+++ SwiftHigherFunctions/ArrayRelated/18_ArrayRemoveLast.swift
@@ -3,14 +3,14 @@ import Foundation
 var arr = [10, 3, 20, 7, 8 , 11, 26]
 print("Actual array: \(arr)")
 
-//Array remove last element and return 
+//Array remove last element and return
 var result = arr.removeLast()
 //result storing the last element
 print("Last element of the array: \(result)")
 
 var arr1 = ["Java","Python","Red","Blue","Green","Purple","Black","Pink"]
 print("Orginal array: \(arr1)")
-//Array remove last two element
+//Array remove last
 var result1 = arr1.removeLast(2)
 print("The last element of the array: \(result1)")
 
